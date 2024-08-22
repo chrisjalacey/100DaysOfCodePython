bbe0b0e00433ce4dca0c3c68f6349f25938f8c70
Chris Lacey <chrisjalacey@outlook.com>
2024-08-22 15:31:03 +0200

    day 1 updated updated with comments

--- Day 1 - Hello World.py
+++ Day 1 - Hello World.py
@@ -1,8 +1,16 @@
+"""
+
+Day 1 - Hello World
+1. Write a program that prints "Hello World!" to the console
+2. Add single line and multi-line comments to your code
+
+Reading List:
+1. How does Python compile and run the code?
+2. Learn the difference between Python and Java
+3. Difference between Compiler and Interpreter
+
+"""
+#prints hello world!
 print ("Hello World!")
 
-#single line comment
-"""
-Multi
-line
-comment
-"""
+
